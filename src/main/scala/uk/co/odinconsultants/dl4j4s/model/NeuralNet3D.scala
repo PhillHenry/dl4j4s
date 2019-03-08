7de8270898836231767af307c8785c97d9e60269
PhillHenry <phillhenry@gmail.com>
2019-03-08 09:10:24 +0000

    Correct dependencies. Test passes. Comment added.

--- src/main/scala/uk/co/odinconsultants/dl4j4s/model/NeuralNet3D.scala
+++ src/main/scala/uk/co/odinconsultants/dl4j4s/model/NeuralNet3D.scala
@@ -4,6 +4,10 @@ import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import uk.co.odinconsultants.dl4j4s.autogen.Nat
 import uk.co.odinconsultants.dl4j4s.shapeless.Ops.ToInt
 
+/**
+  * @tparam X "nIn doesn't depend on time series length at all. nIn for RNNs is the number of values per time step
+  *           similarly for CNNs, it's number of channels/depth (i.e., height/width doesn't matter)" Alex Black, Gitter 03:37, 8 March 2019
+  */
 class NeuralNet3D[T : Numeric, X <: Nat, Y <: Nat, Z <: Nat](val net: MultiLayerNetwork)
                                                             (implicit x: ToInt[X], y: ToInt[Y], z: ToInt[Z]) {
 
--- src/main/scala/uk/co/odinconsultants/dl4j4s/model/NeuralNet3DBuilder.scala
+++ src/main/scala/uk/co/odinconsultants/dl4j4s/model/NeuralNet3DBuilder.scala
@@ -1,25 +1,17 @@
 package uk.co.odinconsultants.dl4j4s.model
 
 import org.deeplearning4j.nn.conf.{GradientNormalization, NeuralNetConfiguration}
-import org.deeplearning4j.nn.conf.layers.{LSTM, RnnOutputLayer}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
 import org.deeplearning4j.nn.weights.WeightInit
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener
-import org.nd4j.linalg.activations.Activation
-import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.learning.config.Nesterovs
-import org.nd4j.linalg.lossfunctions.LossFunctions
-import org.nd4j.linalg.lossfunctions.impl.LossNegativeLogLikelihood
 import uk.co.odinconsultants.dl4j4s.autogen.Nat
 import uk.co.odinconsultants.dl4j4s.shapeless.Ops._
 
 object NeuralNet3DBuilder {
 
-  def apply[T : Numeric, X <: Nat, Y <: Nat, Z <: Nat, H <: Nat](first: LayerBuilder[X, H], second: LayerBuilder[H, Z])(implicit x: ToInt[X], y: ToInt[Y], z: ToInt[Z]) : NeuralNet3D[T, X, Y, Z] = {
-    val nIn:          Int = x.apply()
-    val seriesLength: Int = y.apply()
-    val nClasses:     Int = z.apply()
-
+  def apply[T : Numeric, X <: Nat, Y <: Nat, Z <: Nat, H <: Nat](first: LayerBuilder[X, H], second: LayerBuilder[H, Z])
+                                                                (implicit x: ToInt[X], y: ToInt[Y], z: ToInt[Z]) : NeuralNet3D[T, X, Y, Z] = {
     // TODO make all these other configs configurable
     val conf = new NeuralNetConfiguration.Builder()
       .seed(123)
